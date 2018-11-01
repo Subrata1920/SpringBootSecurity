de074fecd4147f58884a37fda3fe8f685b729aa2
Subrata1920 <>
2018-11-01 13:06:34 +0530

    Database configuration added

---
+++ src/main/java/com/example/sbo/config/Oauth2Configuration.java
@@ -0,0 +1,130 @@
+package com.example.sbo.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+@Configuration
+public class Oauth2Configuration {
+	
+	@Configuration
+	@EnableAuthorizationServer
+	public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter{
+
+		@Autowired
+		private AuthenticationManager authenticationManager;
+		
+		public static final String RESOURCEID = "oauth2-resource";
+		
+		@Autowired
+		private DataSource dataSource;
+
+		/**
+		 * The AuthenticationManager for the password grant.
+		 * 
+		 */
+		@Override
+		public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+			endpoints.authenticationManager(authenticationManager)
+					 .tokenStore(tokenStore());  
+		}
+		
+		/**
+		 * **************Spring Documentation*********************
+		 * 
+		 *  If the Resource Server and Authorization Server are in the same application and you use DefaultTokenServices then you 
+		 *  don't have to think too hard about this because it implements all the necessary interfaces so it is automatically consistent.
+		 *  
+		 *  If your Resource Server is a separate application then you have to make sure you match the capabilities of 
+		 *  the Authorization Server and provide a ResourceServerTokenServices that knows how to decode the tokens correctly. 
+		 *  
+		 *  RemoteTokenServices are convenient if there is not a huge volume of traffic in the Resource Servers (every request has 
+		 *  to be verified with the Authorization Server), or if you can afford to cache the results. 
+		 *  
+		 *  To use the /oauth/check_token endpoint you need to expose it by changing its access rule (default is "denyAll()") 
+		 *  in the AuthorizationServerSecurityConfigurer, e.g.
+		 *  
+		 *  @Override
+			public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+				oauthServer.tokenKeyAccess("isAnonymous() || hasAuthority('ROLE_TRUSTED_CLIENT')")
+						   .checkTokenAccess("hasAuthority('ROLE_TRUSTED_CLIENT')");
+			}
+		 *  
+		 * Configuring both the /oauth/check_token endpoint and the /oauth/token_key endpoint. 
+		 * These two endpoints are protected by HTTP Basic authentication using client credentials.
+		 * 
+		 * Note that:
+			1) This RemoteTokenService will use CheckTokenEndPoint on Authorization Server to validate AccessToken and obtain 
+				Authentication object from it.
+			2) The can be found at AuthorizationServerBaseURL +”/oauth/check_token“
+			3) The Authorization Server can use any TokenStore type [JdbcTokenStore, JwtTokenStore, …] – this won’t affect the 
+				RemoteTokenService or Resource server.
+				
+		 * /oauth/check_token (used by Resource Servers to decode access tokens), and 
+		 * /oauth/token_key (exposes public key for token verification if using JWT tokens).
+		 */
+		
+		@Override
+		public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+			security.tokenKeyAccess("permitAll()")
+					.checkTokenAccess("isAuthenticated()");
+		}
+		
+		@Override
+		public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+			clients.jdbc(dataSource);
+		}
+		/**
+		 * Configuring our TokenStore to access the same database that authorization server uses to store access tokens.
+		 * The reason is that the Resource Server needs to be able to check the validity of the access tokens
+		 * issued by the Authorization Server.
+		 * Save the access_token in "oauth_access_token" table.
+		 * @return
+		 */
+		
+		@Bean
+		public TokenStore tokenStore() {
+	        return new JdbcTokenStore(dataSource);
+	    }
+	}
+	
+	@Configuration
+	@EnableResourceServer
+	public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
+		
+		@Bean(name = "dataSource")
+		public DataSourceAutoConfiguration  gfh() {
+			return null;
+			
+		}
+		
+		@Override
+		public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+			
+			resources.resourceId(AuthorizationServerConfig.RESOURCEID);
+		}
+		
+		@Override
+		public void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests()
+				.antMatchers("/","/home").permitAll()
+				.anyRequest().authenticated();
+		}
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+/** Oauth - populate the oauth_client_details table 
+INSERT INTO oauth_client_details (client_id, client_secret, scope, authorized_grant_types, access_token_validity, additional_information,resource_ids)
+VALUES
+('web', 'secret', 'read', 'authorization_code,password,refresh_token,implicit', '900', '{}','oauth2-resource'); */
+
+/**INSERT INTO 'oauth_example'.'account' ('id', 'password', 'username') VALUES ('	1	', 'dazito.com', 'dazito');*/
